53936f7c18ff80f00278e0fc554a9c4f88e853e9
Desmond-netw <dsquare200@gmail.com>
2025-05-20 12:33:34 -0700

    updated main with new exmaples

--- Go_course_full/Big_problem/main.go
+++ Go_course_full/Big_problem/main.go
@@ -4,9 +4,12 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("\t\vFactorial using Iterative Approach")
-	fmt.Println("\t\v", FactorialIterative(13))
+	fmt.Println("\tFactorial using Iterative Approach")
+	fmt.Println("\t", FactorialIterative(5))
 
-	fmt.Println("\t\vFactorial with Recursive Approach")
-	fmt.Println("\t\v", FactorialRecursive(5))
+	fmt.Println("\tFactorial with Recursive Approach")
+	fmt.Println("\t", FactorialRecursive(0))
+
+	fmt.Println("\tTo The Power function :")
+	fmt.Println("\t", ToThePowerIterative(4, 2))
 }
