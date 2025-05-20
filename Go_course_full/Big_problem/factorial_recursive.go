856cd853273fdf3d9c9e3aa0dbe1d77b8ef62875
Desmond-netw <dsquare200@gmail.com>
2025-05-20 12:34:10 -0700

    changed the formilar of recursive factorial

--- Go_course_full/Big_problem/factorial_recursive.go
+++ Go_course_full/Big_problem/factorial_recursive.go
@@ -8,15 +8,9 @@ func FactorialRecursive(n int) int {
 		return 0
 	}
 
-	if n == 0 {
-		return 1 // return one for factorial of 0
+	if n == 0 || n == 1 {
+		return 1
 	}
 
-	result := FactorialRecursive(n - 1)
-
-	if result > (1<<63-1)/n {
-		return 0
-	}
-
-	return n * result
+	return n * FactorialRecursive(n-1)
 }
