a087ee319a9913dfbcc3c3efeb2ecd422bc06071
Desmond-netw <dsquare200@gmail.com>
2025-05-21 05:15:24 -0700

    added new apps with to the power of function

--- Go_course_full/Big_problem/main.go
+++ Go_course_full/Big_problem/main.go
@@ -4,12 +4,13 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("\tFactorial using Iterative Approach")
+	fmt.Println("\tFactorial using Iterative funcion")
 	fmt.Println("\t", FactorialIterative(5))
 
 	fmt.Println("\tFactorial with Recursive Approach")
 	fmt.Println("\t", FactorialRecursive(0))
 
 	fmt.Println("\tTo The Power function :")
-	fmt.Println("\t", ToThePowerIterative(4, 2))
+	fmt.Println("\t", ToThePowerIterative(-8, 8))
+
 }
--- Go_course_full/Big_problem/to_the_power_Iterative.go
+++ Go_course_full/Big_problem/to_the_power_Iterative.go
@@ -4,15 +4,25 @@ package main
 
 func ToThePowerIterative(n int, power int) int {
 
+	// handle special cases
+	// treate 0^0 as 1
+
 	if power == 0 {
+		return 1
+	}
+
+	// handle negative power
+	if power < 0 {
 		return 0
 	}
 
-	result := 0
-
-	for i := 0; i <= power; i++ {
-		result = n * i
-
+	// results initila value to 1
+	result := 1
+	for i := 0; i < power; i++ {
+		// check before multiplying result
+		result *= n
 	}
+
 	return result
+
 }
