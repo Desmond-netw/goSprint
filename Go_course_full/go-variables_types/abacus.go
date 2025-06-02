e5acbb064d846e3d2020e271338581aa0b15cb48
Desmond-netw <dsquare200@gmail.com>
2025-06-02 20:17:25 +0000

    added main to run the functions

--- Go_course_full/go-variables_types/abacus.go
+++ Go_course_full/go-variables_types/abacus.go
@@ -1,1 +1,7 @@
 // go Abacus app
+package main
+
+
+func Abacus(a int, b int) int {
+	return a/b
+}
---
+++ Go_course_full/go-variables_types/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.println("Abacus divition:" Abacus(8 , 3))
+}
