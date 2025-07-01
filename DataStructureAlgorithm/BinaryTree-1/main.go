820804e80b15189ac9b04bb21175d6cd2d72c6d7
Desmond-netw <dsquare200@gmail.com>
2025-07-01 19:33:05 +0000

    Added instance of tree

--- DataStructureAlgorithm/BinaryTree-1/main.go
+++ DataStructureAlgorithm/BinaryTree-1/main.go
@@ -8,3 +8,15 @@ type TreeNode struct {
 }
 
 // function
+func main() {
+	// crate root and instance of TreeNode
+	root := &TreeNode{data: "R"}
+	nodeA := &TreeNode{data: "A"}
+	nodeB := &TreeNode{data: "B"}
+	nodeC := &TreeNode{data: "C"}
+	nodeD := &TreeNode{data: "D"}
+	nodeE := &TreeNode{data: "E"}
+	nodeF := &TreeNode{data: "F"}
+	nodeG := &TreeNode{data: "G"}
+
+}
